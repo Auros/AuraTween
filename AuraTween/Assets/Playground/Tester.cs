76b199afc2394702e0e46e5e3d8a89be429a29f3
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-03-23 16:32:11 -0400

    Update Tester.cs

--- AuraTween/Assets/Playground/Tester.cs
+++ AuraTween/Assets/Playground/Tester.cs
@@ -14,19 +14,19 @@ namespace AuraTween
             yield return new WaitForSeconds(1);
             var mat = transform.GetComponent<Renderer>().material;
             
-            _tweenManager.Run(
+            /*_tweenManager.Run(
                 Vector3.zero, 
                 Vector3.one * 5, 
                 5f, 
                 v => transform.localPosition = v,
-                Easer.InOutCubic);
+                Easer.InOutCubic);*/
 
             _tweenManager.Run(
                 Color.red, 
                 Color.cyan, 
                 5f, 
                 v => mat.color = v,
-                Ease.InOutCubic.ToInterpolator(),
+                Ease.OutBounce.ToInterpolator(),
                 HSV);
         }
 
